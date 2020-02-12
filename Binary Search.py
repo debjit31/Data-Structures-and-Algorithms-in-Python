b555b04f2e59b548c18bd5281d6aee3bcc54f85d
Debjit <debjitchatterjee31@gmail.com>
2020-02-12 23:38:34 +0530

    BInary Search

---
+++ Binary Search.py
@@ -0,0 +1,22 @@
+arr = []
+def binarySearch (low, high, key): 
+	if high >= low: 
+		mid = low + (high - low)//2
+		if arr[mid] == key: 
+			return mid 
+		elif arr[mid] > key: 
+			return binarySearch(low, mid-1, key) 
+		else: 
+			return binarySearch(mid+1, high, key) 
+	else: 
+		return -1
+
+arr = [int(x) for x in input().split()] 
+arr.sort()
+key = int(input())
+result = binarySearch(0, len(arr)-1, key) 
+
+if result != -1: 
+	print ("Element is present at index %d" % (result+1))
+else: 
+	print ("Element is not present in array")
