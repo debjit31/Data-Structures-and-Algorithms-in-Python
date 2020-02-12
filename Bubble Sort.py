c0d722ebe32622783eab9f3ac7d159174433c37e
Debjit <debjitchatterjee31@gmail.com>
2020-02-12 23:43:25 +0530

    Bubble Sort

---
+++ Bubble Sort.py
@@ -0,0 +1,16 @@
+arr = []
+def bubbleSort():
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
+    print(arr)
+        
+
+
+if __name__ == '__main__':
+    arr = [int(x) for x in input().split()]
+    bubbleSort()
+        
