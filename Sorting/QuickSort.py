73758c3b230d94dbcbc2f9dfcf100e5a52d30624
Debjit <debjitchatterjee31@gmail.com>
2020-02-18 22:24:14 +0530

    Quick Sort

---
+++ Sorting/QuickSort.py
@@ -0,0 +1,22 @@
+def partition(arr, low, high):
+    i = low - 1
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i+=1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr ,low, high)
+
+        quicksort(arr, low, pi-1)
+        quicksort(arr, pi+1, high)
+
+if __name__ == '__main__':
+    arr= []
+    arr =  [int(x) for x in input().split()]
+    quicksort(arr, 0,  len(arr)-1)
+    print(arr)
