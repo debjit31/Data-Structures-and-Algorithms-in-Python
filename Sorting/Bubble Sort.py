485d90e7de15e0215ffcb8e9b5e9abc9b81eced0
Debjit <debjitchatterjee31@gmail.com>
2020-02-20 20:34:58 +0530

    Bubble Sort

--- Sorting/Bubble Sort.py
+++ Sorting/Bubble Sort.py
@@ -3,9 +3,7 @@ def bubbleSort():
     for i in range(0, len(arr)):
         for j in range(0, len(arr)-i-1):
             if arr[j] > arr[j+1]:
-                tmp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = tmp
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     print(arr)
         
 
