60d54ddeee9a8652696142601547d38cf984bdc2
Debjit <debjitchatterjee31@gmail.com>
2020-02-24 19:39:38 +0530

    INFYTQ SAMPLE

---
+++ Queue/sample_ques.py
@@ -0,0 +1,14 @@
+class ClassA:
+         def __init__(self, val1) :
+               self.value = val1
+         def method_a(self) :
+               return 10+self.value
+class ClassB:
+         def __init__(self, val2):
+               self. num=val2
+         def method_b(self, obj):
+               return obj.method_a()+self.num
+
+obj1=ClassA(20)
+obj2=ClassB(30)
+print(obj2.method_b(obj1))
